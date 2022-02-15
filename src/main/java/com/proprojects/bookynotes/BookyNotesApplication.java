d68aba62b1ce950ae1b0b28d0eb08e9af947e978
Rodrigowsky <rmelo.res@gmail.com>
2022-02-15 16:53:53 -0100

    Initial Configuration For BookyNotesSpring

---
+++ src/main/java/com/proprojects/bookynotes/BookyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.proprojects.bookynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proprojects/BookyNotes/BookyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proprojects.BookyNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
